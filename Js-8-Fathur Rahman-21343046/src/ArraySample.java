795f94a1a21d91026de687ea3edcb2a2215990cf
fathur021 <105207844+fathur021@users.noreply.github.com>
2022-10-13 22:37:38 +0700

    Add files via upload

---
+++ Js-8-Fathur Rahman-21343046/src/ArraySample.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * Created by 21343046_Fathur rahman
+ */
+
+public class ArraySample {
+    public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i <100; i++){
+            System.out.print(ages[i]);
+        }
+    }
+}
---
+++ Js-8-Fathur Rahman-21343046/src/ArraySample3.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * Created by 21343046_fathur rahman
+ */
+
+public class ArraySample3 {
+    public static void main(String[] args) {
+        //String array 4 baris x 2 kolom
+        String[][] dogs = {{"Terry", "brown"}, //baris ke 0
+                           {"Kristin", "white"}, //baris ke 1
+                           {"Toby", "gray"}, //baris ke 2
+                           {"Fido", "black"} //baris ke 3
+                          };
+        System.out.println(dogs[0][0]);
+        //mengakses variabel dogs dengan indeks baris 0, indeks kolom 0
+    }
+}
